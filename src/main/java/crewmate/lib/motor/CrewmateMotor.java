095e40d0fd6fab18cfd865b5dfb763776cfd01b5
Modular-Holmes <aneurin.hullah@gmail.com>
2024-05-09 21:54:42 -0400

    Vendor Imports work now

--- src/main/java/crewmate/lib/motor/CrewmateMotor.java
+++ src/main/java/crewmate/lib/motor/CrewmateMotor.java
@@ -7,13 +7,44 @@ enum Controller{
 }
 
 public interface CrewmateMotor {
+    /** 
+     * Base Method to set throttle to a motor
+     * @param throttle
+     */
     public void set(double throttle);
+    /**
+     * Base Method to set voltage to a motor
+     * @param voltage
+     */
     public void setVoltage(double voltage);
 
+    /**
+     * Base Method to get the position of a motor
+     * @return position, in units provided through conversion factors
+     */
     public double getPosition();
+    
+    /** 
+     * Base Method to get the velocity of a motor
+     * @return velocity, in units provide through conversion factors
+    */
     public double getVelocity();
+
+    /**
+     * Base Method to get voltage of a motor
+     * @return Voltage of a motor
+     */
     public double getVoltage();
+
+    /**
+     * Base Method to get the current draw (A) of a motor
+     * @return Amps drawn
+     */
     public double getCurrent();
 
+    /**
+     * Base Method to get the type of motor being used
+     * @return Controller Enum, representing different hardware types
+     */
     public Controller getControllerType();
 }
