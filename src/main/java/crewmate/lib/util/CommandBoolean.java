fc9821eb6ed7d401d9e602b700282de6b2154dc8
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2024-05-14 10:40:49 -0400

    Started UTIl

---
+++ src/main/java/crewmate/lib/util/CommandBoolean.java
@@ -0,0 +1,11 @@
+package crewmate.lib.util;
+
+public class CommandBoolean {
+    boolean trigger = false;
+    Command onTrue;
+    Command onFalse;
+
+    public CommandBoolean(Command onTrue, Command onFalse){
+
+    }
+}
