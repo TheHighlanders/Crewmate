0680984316a872b85b0f1d64c6fe0923e8869fe2
FRC 6201 Driver Station Laptop <frc6201@gmail.com>
2024-05-14 10:52:36 -0400

    Added More Util

--- src/main/java/crewmate/lib/util/CommandBoolean.java
+++ src/main/java/crewmate/lib/util/CommandBoolean.java
@@ -1,11 +1,30 @@
 package crewmate.lib.util;
 
-public class CommandBoolean {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+//Could Create a CrewmateCentral subsystem to control all crewmate functions
+public class CommandBoolean extends SubsystemBase{
     boolean trigger = false;
-    Command onTrue;
-    Command onFalse;
+    Runnable onTrue;
+    Runnable onFalse;
 
-    public CommandBoolean(Command onTrue, Command onFalse){
+    public CommandBoolean(Runnable onTrue, Runnable onFalse){
+        this.onTrue = onTrue;
+        this.onFalse = onFalse;
+    }
 
+    public CommandBoolean(Runnable onEither){
+        this.onFalse = this.onTrue = onEither;
+    }
+
+    public void periodic(boolean update){
+        if(update != trigger){
+            if(update){
+                onTrue.run();
+            } else {
+                onFalse.run();
+            }
+            trigger = update;
+        }
     }
 }
