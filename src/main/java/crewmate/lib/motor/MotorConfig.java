07ae256a4c1cc0117403c65cc6963cbb33d69bde
Modular-Holmes <Modular-Holmes@users.noreply.github.com>
2024-05-15 21:17:46 +0000

    Automatic Spotless format

--- src/main/java/crewmate/lib/motor/MotorConfig.java
+++ src/main/java/crewmate/lib/motor/MotorConfig.java
@@ -46,38 +46,67 @@ public class MotorConfig {
     this.velocityConversionFactor = velocityConversionFactor;
   }
 
-    /**
+  /**
    * Creates a new MotorConfig for a motor
+   *
    * @param canID Can ID of the Motor
    * @param motorType Brushed or Brushless
    * @return MotorConfig
    */
-  public MotorConfig motorBasic(int canID, Type motorType){
-    return new MotorConfig(canID, motorType, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty());
+  public MotorConfig motorBasic(int canID, Type motorType) {
+    return new MotorConfig(
+        canID,
+        motorType,
+        Optional.empty(),
+        Optional.empty(),
+        Optional.empty(),
+        Optional.empty(),
+        Optional.empty(),
+        Optional.empty(),
+        Optional.empty());
   }
 
   /**
-   * Creates a new MotorConfig for a motor with an attached gearbox
-   * Velocity will be in rpm
+   * Creates a new MotorConfig for a motor with an attached gearbox Velocity will be in rpm
+   *
    * @param canID Can ID of the Motor
    * @param motorType Brushed or Brushless
    * @param gearboxRatio Ratio of attached gearboxes (rotation/rotation)
    * @return MotorConfig
    */
-  public static MotorConfig motorBasicGearbox(int canID, Type motorType, double gearboxRatio){
-    return new MotorConfig(canID, motorType, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(gearboxRatio), Optional.of(gearboxRatio/60.0d));
+  public static MotorConfig motorBasicGearbox(int canID, Type motorType, double gearboxRatio) {
+    return new MotorConfig(
+        canID,
+        motorType,
+        Optional.empty(),
+        Optional.empty(),
+        Optional.empty(),
+        Optional.empty(),
+        Optional.empty(),
+        Optional.of(gearboxRatio),
+        Optional.of(gearboxRatio / 60.0d));
   }
 
-  public static MotorConfig motorPID(int canID, Type motorType, double gearboxRatio, double p, double i, double d){
-    return new MotorConfig(canID, motorType, Optional.empty(), Optional.of(p), Optional.of(i), Optional.of(d), Optional.empty(), Optional.of(gearboxRatio), Optional.of(gearboxRatio/60.0d));
+  public static MotorConfig motorPID(
+      int canID, Type motorType, double gearboxRatio, double p, double i, double d) {
+    return new MotorConfig(
+        canID,
+        motorType,
+        Optional.empty(),
+        Optional.of(p),
+        Optional.of(i),
+        Optional.of(d),
+        Optional.empty(),
+        Optional.of(gearboxRatio),
+        Optional.of(gearboxRatio / 60.0d));
   }
 
-  public MotorConfig setReversed(boolean reversed){
+  public MotorConfig setReversed(boolean reversed) {
     this.reversed = Optional.of(reversed);
     return this;
   }
 
-  public MotorConfig setCurrentLimit(double currentLimit){
+  public MotorConfig setCurrentLimit(double currentLimit) {
     this.currentLimit = Optional.of(currentLimit);
     return this;
   }
