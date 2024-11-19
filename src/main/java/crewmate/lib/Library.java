325e5dd7c8ec017241411ed34ee11a5b6b335174
lexm2 <lex.k.moulton@gmail.com>
2024-11-18 23:26:38 -0500

    Add LoggedTunablePID

--- src/main/java/crewmate/lib/Library.java
+++ src/main/java/crewmate/lib/Library.java
@@ -5,9 +5,24 @@ public class Library {
   /**
    * Reports status of library
    *
-   * @return True if library is connected, used to determine if added to project correctly
+   * @return True if library is connected, used to determine if added to project
+   *         correctly
    */
+
   public static boolean connected() {
     return true;
+  }
+
+  /**
+   * Sets tuning mode
+   * 
+   * 
+   */
+  public static void tuningMode(boolean newTuningMode) {
+    Constants.tuningMode = newTuningMode;
+  }
+
+  public class Constants {
+    public static boolean tuningMode = false;
   }
 }
---
+++ src/main/java/crewmate/lib/util/LoggedTunablePID.java
@@ -0,0 +1,105 @@
+package crewmate.lib.util;
+
+import crewmate.lib.motor.CrewmateMotor;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
+import crewmate.lib.Library.Constants;
+
+public class LoggedTunablePID {
+  private static final String tableKey = "TunablePID";
+  private final CrewmateMotor motor;
+  private final String key;
+
+  private boolean hasDefault = false;
+  private double defaultP;
+  private double defaultI;
+  private double defaultD;
+
+  private LoggedDashboardNumber kP;
+  private LoggedDashboardNumber kI;
+  private LoggedDashboardNumber kD;
+
+  private double lastP;
+  private double lastI;
+  private double lastD;
+
+  public LoggedTunablePID(CrewmateMotor motor, String dashboardKey) {
+    this.motor = motor;
+    this.key = tableKey + "/" + dashboardKey;
+  }
+
+  public LoggedTunablePID(CrewmateMotor motor, String dashboardKey, double defaultP, double defaultI, double defaultD) {
+    this(motor, dashboardKey);
+    initDefault(defaultP, defaultI, defaultD);
+  }
+
+  public void initDefault(double defaultP, double defaultI, double defaultD) {
+    if (!hasDefault) {
+      hasDefault = true;
+      this.defaultP = defaultP;
+      this.defaultI = defaultI;
+      this.defaultD = defaultD;
+
+      motor.setP(defaultP);
+      motor.setI(defaultI);
+      motor.setD(defaultD);
+
+      if (Constants.tuningMode) {
+        kP = new LoggedDashboardNumber(key + "/kP", defaultP);
+        kI = new LoggedDashboardNumber(key + "/kI", defaultI);
+        kD = new LoggedDashboardNumber(key + "/kD", defaultD);
+
+        lastP = kP.get();
+        lastI = kI.get();
+        lastD = kD.get();
+      }
+    }
+  }
+
+  public boolean UpdateIfChanged() {
+    if (!hasDefault)
+      return false;
+
+    if (Constants.tuningMode) {
+      double currentP = kP.get();
+      double currentI = kI.get();
+      double currentD = kD.get();
+
+      if (currentP != lastP || currentI != lastI || currentD != lastD) {
+        updatePID();
+        lastP = currentP;
+        lastI = currentI;
+        lastD = currentD;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public void updatePID() {
+    if (!hasDefault)
+      return;
+
+    if (Constants.tuningMode) {
+      motor.setP(kP.get());
+      motor.setI(kI.get());
+      motor.setD(kD.get());
+    } else {
+      motor.setP(defaultP);
+      motor.setI(defaultI);
+      motor.setD(defaultD);
+    }
+  }
+
+  public void bindToScheduler() {
+    CommandScheduler.getInstance().onCommandExecute(
+        (command) -> {
+          this.UpdateIfChanged();
+        });
+  }
+
+  public CrewmateMotor getMotor() {
+    return motor;
+  }
+}
