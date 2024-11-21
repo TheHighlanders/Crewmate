e80cf1efe30e0486a3625f68e92fd62fe2433cb7
FRC-6201 <FRC-6201@users.noreply.github.com>
2024-11-21 20:21:53 +0000

    Automatic Spotless format

--- src/test/java/crewmate/lib/util/CommandBooleanTest.java
+++ src/test/java/crewmate/lib/util/CommandBooleanTest.java
@@ -8,29 +8,29 @@ class CommandBooleanTest {
 
   @Test
   void periodic_OnTrueAndOnFalseRunnables_ExecutesCorrectly() {
-      boolean[] flags = {false, false};
-      CommandBoolean cb = new CommandBoolean(() -> flags[0] = true, () -> flags[1] = true, true);
+    boolean[] flags = {false, false};
+    CommandBoolean cb = new CommandBoolean(() -> flags[0] = true, () -> flags[1] = true, true);
 
-      cb.periodic();
-      assertTrue(flags[0]);
-      assertFalse(flags[1]);
+    cb.periodic();
+    assertTrue(flags[0]);
+    assertFalse(flags[1]);
 
-      flags[0] = false;
-      cb.periodic();
-      assertFalse(flags[0]);
-      assertTrue(flags[1]);
+    flags[0] = false;
+    cb.periodic();
+    assertFalse(flags[0]);
+    assertTrue(flags[1]);
   }
 
   @Test
   void periodic_NoRedundantRunnable_ExecutesCorrectly() {
-      boolean[] flag = {false};
-      CommandBoolean cb = new CommandBoolean(() -> flag[0] = true, () -> {}, true);
+    boolean[] flag = {false};
+    CommandBoolean cb = new CommandBoolean(() -> flag[0] = true, () -> {}, true);
 
-      cb.periodic();
-      assertTrue(flag[0]);
+    cb.periodic();
+    assertTrue(flag[0]);
 
-      flag[0] = false;
-      cb.periodic();
-      assertFalse(flag[0]);
+    flag[0] = false;
+    cb.periodic();
+    assertFalse(flag[0]);
   }
 }
